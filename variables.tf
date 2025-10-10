4c2112363102a8655fe5a7d3eeef95b37705018e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-10 15:29:27 +0100

    fix variable

--- variables.tf
+++ variables.tf
@@ -53,8 +53,8 @@ variable "postgresql_subnet_cidr_blocks" {
 
 variable "additional_subnets" {
   type = map(object({
-    name             = optional(string),
-    address_prefixes = optional(set(string))
+    name             = optional(string, null)
+    address_prefixes = optional(set(string), null)
   }))
   default = {}
 }
